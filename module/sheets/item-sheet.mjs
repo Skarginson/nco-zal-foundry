907d215cf188f7ff98a10259906d36c5ede898de
Skarginson <eldaraes@gmail.com>
2026-04-10 14:30:00 +0200

    Add item sheets: gear, move, contact, tag

---
+++ module/sheets/item-sheet.mjs
@@ -0,0 +1,70 @@
+import { onManageActiveEffect, prepareActiveEffectCategories } from '../helpers/effects.mjs';
+
+/**
+ * Feuille générique pour tous les types d'items NCO.
+ * Chaque type a son propre template HBS.
+ * @extends {ItemSheet}
+ */
+export class NCOItemSheet extends ItemSheet {
+  /** @override */
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      classes: ['nco', 'sheet', 'item'],
+      width: 480,
+      height: 400,
+      tabs: [
+        {
+          navSelector: '.sheet-tabs',
+          contentSelector: '.sheet-body',
+          initial: 'description',
+        },
+      ],
+    });
+  }
+
+  /** @override */
+  get template() {
+    return `systems/neon-city-overdrive/templates/item/item-${this.item.type}-sheet.hbs`;
+  }
+
+  /* -------------------------------------------- */
+
+  /** @override */
+  async getData() {
+    const context = super.getData();
+    const itemData = this.document.toObject(false);
+
+    context.system = itemData.system;
+    context.flags = itemData.flags;
+    context.config = CONFIG.NCO;
+
+    // Enrichissement de la description
+    context.enrichedDescription = await TextEditor.enrichHTML(
+      this.item.system.description,
+      {
+        secrets: this.document.isOwner,
+        async: true,
+        rollData: this.item.getRollData(),
+        relativeTo: this.item,
+      }
+    );
+
+    // Effets actifs
+    context.effects = prepareActiveEffectCategories(this.item.effects);
+
+    return context;
+  }
+
+  /* -------------------------------------------- */
+
+  /** @override */
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    if (!this.isEditable) return;
+
+    html.on('click', '.effect-control', (ev) =>
+      onManageActiveEffect(ev, this.item)
+    );
+  }
+}
